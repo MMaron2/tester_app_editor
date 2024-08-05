60752aa9e4b028aadad611d728b5bb2c0216c05b
MMaron <mateusz.maron12@gmail.com>
2024-08-05 13:08:22 +0200

    wyczyszczenie main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include "widget.h"
 #include <QApplication>
 
-#include <QGridLayout>
-#include <QLayout>
-#include <QFrame>
-#include <QLabel>
-#include <QtGui>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QTreeView>
-#include <QTableWidget>
-#include <qpalette.h>
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
